6e6e0104eee9edb75ef61edae1e5ff0e9dedbb35
Timothée Poisot <tim@poisotlab.io>
2017-07-15 16:10:12 -0400

    :wrench: some tests in demo.jl

--- demo.jl
+++ demo.jl
@@ -8,5 +8,7 @@ GBIF.occurrence("1425221362")
 GBIF.occurrences()
 
 # Search occurrences (with parameters)
-gimme_some_wolves = Dict("scientificName" => "Canis lupus", "rtyui" => 2)
-GBIF.occurrences(gimme_some_wolves)
+gimme_some_wolves = Dict("scientificName" => "Canis lupus", "year" => "2003", "isGeoreferenced" => true, "rtyui" => 2)
+wolves_2003 = GBIF.occurrences(gimme_some_wolves)
+
+map(x -> (x["issues"]), wolves_2003["results"])
