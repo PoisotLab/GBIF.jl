c74eaf86c1a6c019bd17643d47564bef30b0849a
Timothée Poisot <timothee.poisot@umontreal.ca>
2017-07-17 11:40:13 -0400

    :books: link to nb

--- docs/_weave/make.jl
+++ docs/_weave/make.jl
@@ -4,5 +4,4 @@ tangle(Pkg.dir("GBIF", "docs", "_weave", "index.jmd"))
 weave(Pkg.dir("GBIF", "docs", "_weave", "index.jmd"), out_path = Pkg.dir("GBIF", "docs", "index.md"), doctype = "pandoc")
 
 tangle(Pkg.dir("GBIF", "docs", "_weave", "ukbirds.jmd"))
-weave(Pkg.dir("GBIF", "docs", "_weave", "ukbirds.jmd"), out_path = Pkg.dir("GBIF", "docs", "ukbirds.md"))
 convert_doc(Pkg.dir("GBIF", "docs", "_weave", "ukbirds.jmd"), Pkg.dir("GBIF", "docs", "ukbirds.ipynb"))
