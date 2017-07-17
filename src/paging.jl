05b5853cc1343f9ad8cfa4b335194d712c6bef52
Timothée Poisot <timothee.poisot@umontreal.ca>
2017-07-17 13:31:50 -0400

    :bug: warn and do nothing instead of fail when restarting a cleaned object

--- src/paging.jl
+++ src/paging.jl
@@ -18,20 +18,21 @@ end
 """
 function next!(o::Occurrences)
   if o.cleaned
-    error("A filtered list of occurences cannot be resumed")
-  end
-  if length(o.occurrences) == o.count
-    info("All occurences for this query have been returned")
+    warn("A filtered list of occurences cannot be resumed - object unchanged")
   else
-    o.query["offset"] = length(o.occurrences)
-    o.query["limit"] = get(o.query, "limit", 20)
-    if (o.query["offset"] + o.query["limit"]) > o.count
-      o.query["limit"] = o.count - o.query["offset"]
+    if length(o.occurrences) == o.count
+      info("All occurences for this query have been returned")
+    else
+      o.query["offset"] = length(o.occurrences)
+      o.query["limit"] = get(o.query, "limit", 20)
+      if (o.query["offset"] + o.query["limit"]) > o.count
+        o.query["limit"] = o.count - o.query["offset"]
+      end
+      get_next = GBIF.occurrences(o.query)
+      append!(o.occurrences, get_next.occurrences)
+      o.offset = length(o.occurrences)
+      @assert o.offset == length(o.occurrences)
     end
-    get_next = GBIF.occurrences(o.query)
-    append!(o.occurrences, get_next.occurrences)
-    o.offset = length(o.occurrences)
-    @assert o.offset == length(o.occurrences)
   end
 end
 
