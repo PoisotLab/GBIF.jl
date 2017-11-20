7ac94538a23102acd320bd45c8f2e2fad57bb63c
Timothée Poisot <timothee.poisot@umontreal.ca>
2017-11-20 10:54:15 -0500

    :white_check_mark: updating tests

--- test/qualitycontrol.jl
+++ test/qualitycontrol.jl
@@ -6,11 +6,4 @@ module TestQAQC
   set = occurrences()
   qualitycontrol!(set)
 
-  @test set.cleaned
-  @test_warn "A filtered list of occurences cannot be resumed - object unchanged" next!(set)
-  @test_warn "A filtered list of occurences cannot be resumed - object unchanged" complete!(set)
-
-  restart!(set)
-  @test set.cleaned == false
-
 end
