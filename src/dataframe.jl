b788e280957a8b1fb3a6451fe5acdf09256e39ab
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-01-07 12:00:34 -0500

    :sparkles: convert method for DataFrames

--- src/dataframe.jl
+++ src/dataframe.jl
@@ -6,8 +6,17 @@ import DataFrames.DataFrame
 function DataFrame(o::Occurrences)
   d = DataFrame()
   for f in fieldnames(Occurrence)
-    _tmp = map(x -> getfield(x, f) == nothing ? NA : getfield(x, f), o)
+    _tmp = map(x -> getfield(x, f) == nothing ? missing : getfield(x, f), o)
     d[Symbol(f)] = _tmp
   end
   return d
+end
+
+import Base.convert
+
+"""
+**Convert a series of occurrences into a DataFrame** 
+"""
+function convert(::Type{DataFrame}, o::Occurrences)
+  return DataFrame(o)
 end
