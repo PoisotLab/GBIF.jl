2f5e61e0893f285c477029d0c9a9628f08566080
Timothée Poisot <timothee.poisot@umontreal.ca>
2019-02-23 11:31:23 -0500

    :books: update taxon documentation

--- src/taxon.jl
+++ src/taxon.jl
@@ -1,5 +1,28 @@
 """
 **Get information about a taxon at any level**
+
+    taxon(name::String)
+
+This function will look for a taxon by its (scientific) name in the GBIF
+reference taxonomy.
+
+Optional arguments are
+
+- `rank::Union{Symbol,Nothing}=:SPECIES` -- the rank of the taxon you want. This
+  is part of a controlled vocabulary, and can only be one of `:DOMAIN`,
+  `:CLASS`, `:CULTIVAR`, `:FAMILY`, `:FORM`, `:GENUS`, `:INFORMAL`, `:ORDER`,
+  `:PHYLUM,`, `:SECTION`, `:SUBCLASS`, `:VARIETY`, `:TRIBE`, `:KINGDOM`,
+  `:SUBFAMILY`, `:SUBFORM`, `:SUBGENUS`, `:SUBKINGDOM`, `:SUBORDER`,
+  `:SUBPHYLUM`, `:SUBSECTION`, `:SUBSPECIES`, `:SUBTRIBE`, `:SUBVARIETY`,
+  `:SUPERCLASS`, `:SUPERFAMILY`, `:SUPERORDER`, and `:SPECIES`
+
+- `strict::Bool=true` -- whether the match should be strict, or fuzzy
+
+- `verbose::Bool=false` -- whether the query should print out information about the reply (not usually useful)
+
+Finally, one can also specify other levels of the taxonomy, using  `kingdom`,
+`phylum`, `class`, `order`, `family`, and `genus`, all of which can either be
+`String` or `Nothing`.
 """
 function taxon(name::String;
    rank::Union{Symbol,Nothing}=:SPECIES, strict::Bool=true, verbose::Bool=false,
