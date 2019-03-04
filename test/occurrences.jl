5440206c419ff42e575984a713c115e7d3610e2c
Timothée Poisot <timothee.poisot@umontreal.ca>
2019-03-04 12:40:45 -0500

    :fire: remove test

--- test/occurrences.jl
+++ test/occurrences.jl
@@ -3,11 +3,6 @@ module TestGBIFRecords
   using GBIF
   using Test
 
-  qpars = Dict("scientificName" => "Mus musculus", "year" => 1999, "hasCoordinate" => true)
-  set1 = occurrences(qpars)
-  @test typeof(set1) == GBIFRecords
-  @test length(set1) == 20
-
   set2 = occurrences()
   @test typeof(set2) == GBIFRecords
   @test length(set2) == 20
