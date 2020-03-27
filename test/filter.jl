a801fc3ec83a6d79123b353c5fade536ad0de44e
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-03-26 22:20:29 -0400

    :tada: almost ready for PR
    lots of old functions removed
    API streamlined
    tests pass

--- test/filter.jl
+++ test/filter.jl
@@ -14,23 +14,25 @@ module TestQAQC
   allrecords!(set)
   @test length(set) == l1
 
-  # Using next! and complete! after filter! should work correctly
+  # Using occurrences! after filter! should work correctly
   filter!(have_no_issues, set)
-  next!(set)
+  occurrences!(set)
   @test length(set) == 2*l1
 
   # Using next! and complete! after filter! should work correctly
   filter!(have_no_issues, set)
   allrecords!(set)
-  next!(set)
+  occurrences!(set)
   @test length(set) == 3*l1
 
   # Multiple filters at once
   allrecords!(set)
-  filter!.([have_ok_coordinates, have_neither_zero_coordinates, have_a_date], set)
+  #filter!.([have_ok_coordinates, have_neither_zero_coordinates, have_a_date], set)
+  filter!.([have_ok_coordinates, have_a_date], set)
+  allrecords!(set)
+  filter!.([have_neither_zero_coordinates, have_a_date], set)
+  allrecords!(set)
+  filter!.([have_neither_zero_coordinates, have_a_date], set)
   @test length(set) != 3*l1
-
-  # Deprecation for qualitycontrol!
-  # @test_warn "deprecated" qualitycontrol!(set; verbose=false)
 
 end
