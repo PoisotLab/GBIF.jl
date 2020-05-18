fb9f2ffa574d6cb074dfb75a315fc7570c6ed25a
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-05-17 23:01:09 -0400

    Update paging.jl

--- src/paging.jl
+++ src/paging.jl
@@ -3,7 +3,8 @@
 
 This function will retrieve the next page of results. By default, it will walk
 through queries 20 at a time. This can be modified by changing the
-`.query["limit"]` value, to any value *below* 200.
+`.query["limit"]` value, to any value *below* 300, which is the limit set
+by GBIF for the queries.
 
 If filters have been applied to this query before, they will be *removed* to
 ensure that the previous and the new occurrences have the same status.
